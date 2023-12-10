ec7a9fcce6155984afe56faafd23d6f973f7d30a
SAudrius <121971874+SAudrius@users.noreply.github.com>
2023-12-10 20:04:15 +0200

    task 8 done with logs

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  sum(num1, num2) {
+    return num1 + num2;
+  }
+  subtraction(num1, num2) {
+    return num1 - num2;
+  }
+  multiplication(num1, num2) {
+    return num1 * num2;
+  }
+  division(num1, num2) {
+    return num1 / num2;
+  }
+}
+const calc1 = new Calculator();
+calc1.sum(5, 15);
+calc1.subtraction(5, 15);
+calc1.multiplication(5, 15);
+calc1.division(15, 5);
+// console.log("calc1.sum(5, 15) ===", calc1.sum(5, 15));
+// console.log("calc1.subtraction(5, 15) ===", calc1.subtraction(5, 15));
+// console.log("calc1.multiplication(5, 15) ===", calc1.multiplication(5, 15));
+// console.log("calc1.division(15, 5) ===", calc1.division(15, 5));
